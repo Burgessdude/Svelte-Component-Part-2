16db5bd2235bdd18ed92e766332a66681727b50e
Burgessdude <143647882+Burgessdude@users.noreply.github.com>
2024-09-19 19:49:33 -0600

    Added new Repository

---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+
+<script>
+	import MyButton from "./Button.svelte";
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"/>
+</svelte:head>
+
+
+<section>
+	<MyButton name="Button" type="regular" size="md"/>
+	<br>
+	<br>
+	<MyButton name="Button" type="flat" size="md"/>
+	<br>
+	<br>
+	<MyButton name="Button" type="disabled" size="md"/>
+
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
---
+++ src/routes/Button.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let name = "Default";
+	export let type = "regular"; // regular, flat, disabled
+	export let size = "md"; // sm, md, lg
+	const sizes = {
+		"sm" : "px-1 py-3 pr-8 pl-8",
+		"md" : "px-5 py-3 pr-8 pl-8",
+		"lg" : "px-7 py-5 pr-8 pl-8",
+	}
+</script>
+
+{#if type=="regular"}
+ <button class={`bg-indigo-500 text-white font-medium h-12 w-80 rounded-full ${(size in sizes) ? sizes[size] : sizes["md"]}`}>
+  {name}
+ </button>
+{/if}
+
+{#if type=="flat"}
+ <button class={`bg-indigo-700 text-white font-medium h-12 w-80 rounded-full text-center align-middle ${(size in sizes) ? sizes[size] : sizes["md"]}`}>
+  {name}
+ </button>
+{/if}
+
+{#if type=="disabled"}
+ <button class={`bg-gray-400 text-gray-600 font-medium h-12 w-80 rounded-full ${(size in sizes) ? sizes[size] : sizes["md"]}`}>
+  {name}
+ </button> 
+{/if}
